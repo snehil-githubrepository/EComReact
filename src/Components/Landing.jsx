2e05740859a67ab8a6eba2a43d036408e7f5ab86
Snehil <snehil.sharma020901@gmail.com>
2024-10-30 03:51:18 +0530

    landing change

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 function Landing() {
   return (
-    <div className="flex items-center justify-center font-semibold h-screen bg-gray-200 text-3xl font-sans">
-      <div>Welcome To Products Page E commerce</div>
-      <span className="font-serif">
-        <h1>Navigate to /login or /signup</h1>
-      </span>
+    <div className="flex justify-center h-screen bg-gray-100 text-gray-800 font-sans p-6">
+      <div className="max-w-2xl h-56 shadow-lg p-6 rounded-lg bg-gray-200">
+        <h1 className="text-5xl font-extrabold text-sky-900 mb-6">
+          Discover Your Next Favorite Product
+        </h1>
+        <p className="text-slate-600 text-lg">
+          Discover the all-new products in this wide range of options available
+          just for you!
+        </p>
+      </div>
     </div>
   );
 }
