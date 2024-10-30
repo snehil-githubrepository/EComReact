38c34f23fdbd16a2d797a858e895fb843ecffca4
Snehil <snehil.sharma020901@gmail.com>
2024-10-31 05:26:55 +0530

    image change

--- src/Components/Landing/TrendingNowBanner.jsx
+++ src/Components/Landing/TrendingNowBanner.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 
 function TrendingNowBanner() {
   const images = [
-    "https://png.pngtree.com/thumb_back/fh260/background/20231110/pngtree-happy-diwali-festival-of-light-background-with-diwali-diya-image_14005021.jpg",
-    "https://images.pexels.com/photos/3656267/pexels-photo-3656267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/4078516/pexels-photo-4078516.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://as2.ftcdn.net/v2/jpg/09/82/19/37/1000_F_982193797_n3i49v5T6Jb1HaxNcjd3TQmN5LFMNyUn.jpg",
+    "https://c4.wallpaperflare.com/wallpaper/308/395/819/religious-diwali-candle-hd-wallpaper-preview.jpg",
+    "https://t3.ftcdn.net/jpg/06/39/60/72/240_F_639607204_i7LgxE13yXzbPx1t4ygivMBIOlJFiYV7.jpghttps://t3.ftcdn.net/jpg/06/39/60/72/240_F_639607274_BUs4CKcdGmFLgU9FAch24sPbQ1zVfIdb.jpg",
   ];
 
   const [currentImage, setCurrentImage] = useState(0);
