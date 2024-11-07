ae8016dc9b9e47e80c9ca315e13e595eac597985
Snehil <snehil.sharma020901@gmail.com>
2024-11-07 17:13:34 +0530

    custom styling

--- src/Components/Common/Button.jsx
+++ src/Components/Common/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Button({ label, onClick }) {
+function Button({ label, onClick, className = "" }) {
   return (
     <div className="flex justify-center">
       <button
         onClick={onClick}
-        className="bg-sky-700 text-white font-medium py-2 px-6 rounded-lg shadow-md hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 transition duration-200"
+        className={`bg-sky-700 text-white font-medium py-2 px-6 rounded-lg shadow-md hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 transition duration-200 ${className}`}
       >
         {label}
       </button>
